defa3c7578ad8008dd008b198bc83b0d1270cdc3
basundhara <basundharasaha12398@gmail.com>
2023-01-10 13:02:10 +0530

    initial Commit

---
+++ CrudEFCoreWithSwagger/CrudEFCoreWithSwagger/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEFCoreWithSwagger.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Roll { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
